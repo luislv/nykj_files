5220f12e413ac305c98f3058927537a286d36e8d
luislv <1472116@qq.com>
2015-06-17 11:37:12 +0800

    客服呼叫中心 初始文档提交

---
+++ 需求和图纸/客服呼叫中心后台/客服需求/客服呼叫中心V5.4（Auxre页面）/files/报表统计（一期_）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(),r,_(s,t,u,v,w,_(x,y,z,A),B,null,C,D,E,D,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(R,_(S,T,U,[_(S,V,W,g,X,[_(Y,Z,S,ba,bb,bc),_(Y,bd,S,be,bf,[])])])),bg,_(bh,[])),bi,_(),bj,_());}; 
+var b="url",c="报表统计（一期_）.html",d="generationDate",e=new Date(1433398943403.26),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="f36f5cebea7a4a259d7b1d46f9d4e8fa",m="type",n="Axure:Page",o="name",p="报表统计（一期 ）",q="notes",r="style",s="baseStyle",t="627587b6038d43cca051c114ac41ad32",u="pageAlignment",v="center",w="fill",x="fillType",y="solid",z="color",A=0xFFFFFFFF,B="image",C="imageHorizontalAlignment",D="near",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="onLoad",S="description",T="OnLoad",U="cases",V="用例 1",W="isNewIfGroup",X="actions",Y="action",Z="wait",ba="等待3000毫秒",bb="waitTime",bc=3000,bd="fadeWidget",be="显示/隐藏元件",bf="objectsToFades",bg="diagram",bh="objects",bi="masters",bj="objectPaths";
+return _creator();
+})());
